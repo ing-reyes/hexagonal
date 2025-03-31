3a250aa54a369d61e3cd04ef272a29b64b99bcb9
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-30 20:49:41 -0400

    Refactor error handling method to be an instance method in Handler class

--- src/common/errors/handler.error.ts
+++ src/common/errors/handler.error.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 import { ManagerError } from "./manager.error";
 
 export class Handler {
-    static error(error: unknown, res: Response) {
+    error(error: unknown, res: Response) {
         if (error instanceof ManagerError) {
             res.status(error.statusCode).json({ statusCode: error.statusCode, message: error.message, statusMsg: error.statusMsg });
             return;
