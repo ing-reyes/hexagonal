15d265dd50fdb40cb8afd6910950a19f7dcdc07d
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-30 22:11:32 -0400

    Add UsersMiddleware to validate user creation and update requests

---
+++ src/users/infraestructure/presentation/middlewares/users.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { CreateUserDto } from "../../../application/dtos/create-user.dto";
+import { UpdateUserDto } from "../../../application/dtos/update-user.dto";
+import { injectable } from "inversify";
+
+@injectable()
+export class UsersMiddleware {
+    validateCreateUse = (req: Request, res: Response, next: NextFunction) => {
+        const [error, createUserDto] = CreateUserDto.create(req.body);
+        if (error) {
+            res.status(400).json({ statusCode: 400, message: error, statusMsg: 'BAD_REQUEST' });
+            return;
+        }
+
+        //* Add the createUserDto to the request body for later use in the controller
+        req.body.createUserDto = createUserDto;
+
+        next()
+
+    }
+
+    validateUpdateUse = (req: Request, res: Response, next: NextFunction) => {
+        const [error, updateUserDto] = UpdateUserDto.update(req.body);
+        if (error) {
+            res.status(400).json({ statusCode: 400, message: error, statusMsg: 'BAD_REQUEST' });
+            return;
+        }
+
+        //* Add the updateUserDto to the request body for later use in the controller
+        req.body.updateUserDto = updateUserDto;
+
+        next()
+
+    }
+}
