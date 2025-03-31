b74bee630fdab2c06c174ff4e7d737625597999a
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-31 02:32:47 -0400

    Add logging functionality to the server startup process

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -6,6 +7,7 @@ import { CORS } from './common/constants/cors';
 import { RoutesFactory } from './routes.factory';
 import { inject, injectable, unmanaged } from 'inversify';
 import { TYPES } from './common/types/inversify.type';
+import { Logger } from './common/logging/logger';
 
 interface Options {
     port: number;
@@ -15,6 +17,7 @@ interface Options {
 export class Server {
     private readonly app: express.Application = express();
     private readonly port: number;
+    private readonly logger = new Logger(Server.name)
 
     constructor(
         @unmanaged()
@@ -29,7 +32,7 @@ export class Server {
     start() {
         //* Enable Cors
         this.app.use(cors(CORS));
-        
+
         //* Middlewares
         this.app.use(morgan('dev')); // Log requests to the console
         this.app.use(express.json()); // Parse JSON request body
@@ -40,8 +43,23 @@ export class Server {
 
         //* Start server
         this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`);
+            this.logger.log(`Application running on port ${this.port}`);
+            this.logger.log(`Application running in ${process.env.NODE_ENV?.trim()} mode`);
+            this.logger.log(`Application base URL: ${this.baseUrl()}`);
         }
         );
+    }
+
+    private baseUrl(){
+        const protocol = process.env.NODE_ENV?.trim() === 'production' ? 'https' : 'http';
+        const hostname = os.hostname(); // Get the hostname of the operating system
+
+        if (hostname === 'localhost' || hostname === '127.0.0.1') {
+            return `${protocol}://${hostname}:${this.port}`;
+        }
+
+        // If it's not localhost, assume it's a production domain.
+        // If you have a specific domain, you can use process.env.DOMAIN or similar.
+        return `${protocol}://${hostname}:${this.port}`;
     }
 }
