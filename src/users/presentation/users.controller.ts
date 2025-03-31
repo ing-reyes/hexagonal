9fc52480a8eb76d82aee45255d5327372038f2d5
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-30 20:50:23 -0400

    Refactor UsersController to use Inversify for dependency injection and streamline use case execution

--- src/users/presentation/users.controller.ts
+++ src/users/presentation/users.controller.ts
@@ -1,37 +1,41 @@
 import { Request, Response } from "express";
 
 import { CreateUserUseCase } from "../application/use-cases/create-user.use-case";
-import { CreateUserDto } from "../application/dtos/create-user.dto";
 import { FindAllUsersUseCase } from "../application/use-cases/find-all-users.use-case";
 import { FindOneUserUseCase } from "../application/use-cases/find-one-user.use-case";
 import { Handler } from "../../common/errors/handler.error";
 import { PaginationDto } from "../../common/dtos/pagination/pagination.dto";
 import { RemoveUserUseCase } from "../application/use-cases/remove-user.use-case";
 import { UpdateUserUseCase } from "../application/use-cases/update-user.use-case";
-import { UpdateUserDto } from "../application/dtos/update-user.dto";
-import { UserRepository } from "../domain/repositories/user.repository";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../../common/types/inversify.type";
 
 
-type HandlerError = (error: unknown, res: Response) => void;
 
+@injectable()
 export class UsersController {
 
     constructor(
-        private readonly userRepository: UserRepository,
-        private readonly handlerError: HandlerError = Handler.error // Error Handler
+        @inject(TYPES.Handler)
+        private readonly handlerError: Handler, // Error Handler
+        @inject(TYPES.CreateUserUseCase)
+        private readonly createUserUseCase: CreateUserUseCase,
+        @inject(TYPES.FindAllUsersUseCase)
+        private readonly findAllUsersUseCase: FindAllUsersUseCase,
+        @inject(TYPES.FindOneUserUseCase)
+        private readonly findOneUserUseCase: FindOneUserUseCase,
+        @inject(TYPES.UpdateUserUseCase)
+        private readonly updateUserUseCase: UpdateUserUseCase,
+        @inject(TYPES.RemoveUserUseCase)
+        private readonly removeUserUseCase: RemoveUserUseCase,
     ) { }
 
     create = (req: Request, res: Response) => {
-        const [error, createUserDto] = CreateUserDto.create(req.body);
-        if (error) {
-            res.status(400).json({ statusCode: 400, message: error, statusMsg: 'BAD_REQUEST' });
-            return;
-        }
 
         // Use Case
-        new CreateUserUseCase(this.userRepository).execute(createUserDto!)
+         this.createUserUseCase.execute(req.body.createUserDto)
             .then(users => res.json(users))
-            .catch(error => this.handlerError(error, res));
+            .catch(error => this.handlerError.error(error, res));
     }
 
     findAll = (req: Request, res: Response) => {
@@ -42,37 +46,32 @@ export class UsersController {
         }
 
         // Use Case
-        new FindAllUsersUseCase(this.userRepository).execute(paginationDto!)
+        this.findAllUsersUseCase.execute(paginationDto!)
             .then(users => res.json(users))
-            .catch(error => this.handlerError(error, res));
+            .catch(error => this.handlerError.error(error, res));
     }
 
     findOne = (req: Request, res: Response) => {
 
         // Use Case
-        new FindOneUserUseCase(this.userRepository).execute(req.params.id)
+        this.findOneUserUseCase.execute(req.params.id)
             .then(users => res.json(users))
-            .catch(error => this.handlerError(error, res));
+            .catch(error => this.handlerError.error(error, res));
     }
 
     update = (req: Request, res: Response) => {
-        const [error, updateUserDto] = UpdateUserDto.update(req.body);
-        if (error) {
-            res.status(400).json({ statusCode: 400, message: error, statusMsg: 'BAD_REQUEST' });
-            return;
-        }
-
+    
         // Use Case
-        new UpdateUserUseCase(this.userRepository).execute(req.params.id, updateUserDto!)
+        this.updateUserUseCase.execute( req.params.id, req.body.updateUserDto)
             .then(users => res.json(users))
-            .catch(error => this.handlerError(error, res));
+            .catch(error => this.handlerError.error(error, res));
     }
 
     remove = (req: Request, res: Response) => {
 
         // Use Case
-        new RemoveUserUseCase(this.userRepository).execute(req.params.id)
+        this.removeUserUseCase.execute(req.params.id)
             .then(users => res.json(users))
-            .catch(error => this.handlerError(error, res));
+            .catch(error => this.handlerError.error(error, res));
     }
 }
