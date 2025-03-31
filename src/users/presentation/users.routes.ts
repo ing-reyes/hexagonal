f98f9153f0988df7fd23fc63fab2c1e55aeaba11
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-30 20:50:30 -0400

    Refactor UsersRoutes to use Inversify for dependency injection and integrate UsersMiddleware for request validation

--- src/users/presentation/users.routes.ts
+++ src/users/presentation/users.routes.ts
@@ -1,25 +1,39 @@
+import { inject, injectable } from "inversify";
 import { Router } from "express";
-
+import { TYPES } from "../../common/types/inversify.type";
 import { UsersController } from "./users.controller";
-import { UserDatasourceImpl } from "../infraestructure/datasources/user.datasource.impl";
-import { UserRepositoryImpl } from "../infraestructure/repositories/user.repository.impl";
+import { UsersMiddleware } from "./middlewares/users.middleware";
 
+
+@injectable()
 export class UsersRoutes {
 
-    static get routes(): Router {
+    constructor(
+        @inject(TYPES.UsersController)
+        private readonly usersController: UsersController,
+        @inject(TYPES.UsersMiddleware)
+        private readonly usersMiddleware: UsersMiddleware,
+    ){}
+
+    get routes(): Router {
         const routes = Router();
 
-        // Dependency Injection
-        const usersDatasource = new UserDatasourceImpl()
-        const usersReposotory = new UserRepositoryImpl(usersDatasource)
-        const controller = new UsersController(usersReposotory);
-
         // localhost:3000/api/users
-        routes.post('/', controller.create);
-        routes.get('/', controller.findAll);
-        routes.get('/:id', controller.findOne);
-        routes.patch('/:id', controller.update);
-        routes.delete('/:id', controller.remove);
+        routes.post('/', 
+            [this.usersMiddleware.validateCreateUse],
+            this.usersController.create
+        );
+        
+        routes.get('/', this.usersController.findAll);
+        
+        routes.get('/:id', this.usersController.findOne);
+        
+        routes.patch('/:id',
+            [this.usersMiddleware.validateUpdateUse], 
+            this.usersController.update
+        );
+        
+        routes.delete('/:id', this.usersController.remove);
 
         return routes;
     }
