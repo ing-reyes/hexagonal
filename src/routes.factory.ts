1c833dc822e0e1e0d3f224cba2caf6b298aadc20
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-03-30 20:50:44 -0400

    Add RoutesFactory for centralized route management with Inversify integration

---
+++ src/routes.factory.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { UsersRoutes } from './users/presentation/users.routes';
+import { inject, injectable } from "inversify";
+import { TYPES } from "./common/types/inversify.type";
+
+@injectable()
+export class RoutesFactory {
+
+    constructor(
+        @inject(TYPES.UsersRoutes)
+        private readonly usersRoutes: UsersRoutes
+    ){}
+
+    get routes(): Router {
+        const routes = Router();
+
+        routes.use('/api/users', this.usersRoutes.routes);
+
+        return routes;
+    }
+}
