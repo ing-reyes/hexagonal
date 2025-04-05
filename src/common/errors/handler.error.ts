adc8e1f24a5c0059e226714a3c31c23757367862
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-04 22:26:28 -0400

    Add logging functionality to error handler with Logger and SaveLogger

--- src/common/errors/handler.error.ts
+++ src/common/errors/handler.error.ts
@@ -1,14 +1,23 @@
 import { Response } from "express";
 
+import { Logger } from "../logging/logger";
 import { ManagerError } from "./manager.error";
+import { injectable } from "inversify";
+import { SaveLogger } from "../logging/save-logger";
 
+@injectable()
 export class Handler {
+    private readonly logger = new Logger(Handler.name);
+    private readonly saveLogger = new SaveLogger(Handler.name);
+
     error(error: unknown, res: Response) {
         if (error instanceof ManagerError) {
             res.status(error.statusCode).json({ statusCode: error.statusCode, message: error.message, statusMsg: error.statusMsg });
             return;
         }
 
+        this.logger.error(error);
+        this.saveLogger.error(error, 'handler.error.ts');
         res.status(500).json({ statusCode: 500, message: 'Internal Server Error', statusMsg: 'INTERNAL_SERVER_ERROR' });
     }
 }
