b50d97753a95386880b34433cb467d9db953972e
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-04 22:25:41 -0400

    Add user model schema for MongoDB with validation and transformation

---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+import { UserRole } from "../../../users/domain/enums/user.role";
+
+export const userSchema = new Schema({
+    name: {
+        type: String,
+        require: [true, 'Name is required'],
+    },
+    email: {
+        type: String,
+        require: [true, 'Email is required'],
+    },
+    password: {
+        type: String,
+        require: [true, 'Password is required'],
+    },
+    roles: {
+        type: [String],
+        enum: Object.values(UserRole),
+        default: [UserRole.USER],
+    },
+});
+
+userSchema.set('toJSON', {
+    virtuals: false,
+    versionKey: false,
+    transform: (document, returnedObject) => {
+        returnedObject.id = document._id
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.password
+    }
+})
+
+export const UserModel = mongoose.model('User', userSchema);
