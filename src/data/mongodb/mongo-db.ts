60cc1cc23311aca19a4fdc45254c4ee6ed013b61
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-04 22:25:13 -0400

    Add MongoDB connection class with error handling and logging

---
+++ src/data/mongodb/mongo-db.ts
@@ -0,0 +1,30 @@
+import { injectable } from "inversify";
+import { ManagerError } from "../../common/errors/manager.error";
+import { Logger } from '../../common/logging/logger';
+import mongoose from "mongoose";
+import { SaveLogger } from "../../common/logging/save-logger";
+
+interface Options {
+    mongoUrl: string;
+    dbName: string;
+}
+
+@injectable()
+export class MongoDB {
+    private readonly saveLogger = new SaveLogger(MongoDB.name)
+    private readonly logger = new Logger(MongoDB.name)
+
+    async connect(options: Options) {
+        const { dbName, mongoUrl } = options;
+        try {
+            await mongoose.connect(mongoUrl, { dbName });
+
+            this.logger.log('Mongo Database Connected', MongoDB.name);
+            return true;
+        } catch (error) {
+            this.logger.error('Mongo Connection Error', MongoDB.name);
+            this.saveLogger.error('Mongo Database Connection Error', 'mongo-db.ts');
+            throw ManagerError.requestTimeout('The servers are too busy to serve the request');
+        }
+    }
+}
