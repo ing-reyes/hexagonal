9f05b031198e2dc97f438be6cbbe2a75c0c6b8c4
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-04 22:26:37 -0400

    Add UserMapper for transforming user model to entity with validation

---
+++ src/users/infraestructure/mappers/user.mapper.ts
@@ -0,0 +1,17 @@
+import { ManagerError } from "../../../common/errors/manager.error";
+import { User } from "../../domain/entities/user.entity";
+
+export class UserMapper {
+
+    static fromUserModelToEntity( object: {[key:string]: any} ): User{
+        const { id, _id, name, email, password, roles } = object;
+        
+        if( !id && !_id ) throw ManagerError.badRequest('Missing id');
+        if( !name ) throw ManagerError.badRequest('Missing name');
+        if( !email ) throw ManagerError.badRequest('Missing email');
+        if( !password ) throw ManagerError.badRequest('Missing password');
+        if( !roles ) throw ManagerError.badRequest('Missing roles');
+
+        return new User(id || _id, name, email, password, roles);
+    }
+}
