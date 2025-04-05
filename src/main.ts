3299ee4b9c5a8c668b542d95f285cfe8bd6a5ecb
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-04 22:26:54 -0400

    Initialize MongoDB connection in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { envs } from './common/config/envs.config';
 import { container } from './common/config/inversify.config';
 import { TYPES } from './common/types/inversify.type';
+import { MongoDB } from './data/mongodb/mongo-db';
 import { Server } from './server';
 
-function bootstrap() {
+async function bootstrap() {
 
-    //todo: Initialize the Database
+    //* Initialize the Database
+    const mongoDB = container.get<MongoDB>(TYPES.MongoDB);
+    await mongoDB.connect({ 
+        mongoUrl: envs.MONGO_DB_URL, 
+        dbName: envs.MONGO_DB_NAME 
+    });
 
     //* Initialize the server
     const server = container.get<Server>(TYPES.Server);
