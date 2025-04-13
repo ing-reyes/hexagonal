36f16023ac5ed8a29cedc2f96564d603025107c7
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-13 19:07:02 -0400

    Add REDIS_URL configuration to envs.config.ts

--- src/common/config/envs.config.ts
+++ src/common/config/envs.config.ts
@@ -12,4 +12,6 @@ export const envs = {
     // MongoDB
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
     MONGO_DB_URL: get('MONGO_DB_URL').required().asString(),
+
+    REDIS_URL: get('REDIS_URL').required().asString(),
 }
