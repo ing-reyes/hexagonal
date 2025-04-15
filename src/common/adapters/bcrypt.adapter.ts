aaff8b91383ea8aed6a0c14a3b6a782fbc823eae
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-15 17:57:23 -0400

    Add injectable decorator to BcryptAdapter for dependency injection

--- src/common/adapters/bcrypt.adapter.ts
+++ src/common/adapters/bcrypt.adapter.ts
@@ -1,5 +1,7 @@
 import { hashSync, compareSync } from 'bcryptjs'
+import { injectable } from 'inversify'
 
+@injectable()
 export class BcryptAdapter {
     hash(password: string): string {
         return hashSync(password, 10)
