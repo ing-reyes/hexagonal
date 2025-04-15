aa4a283d09df384b843d2862614a96504a14e9c1
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-15 17:57:43 -0400

    Add AuthDatasource and AuthRepository abstractions for user authentication

---
+++ src/auth/domain/datasources/auth.datasource.ts
@@ -0,0 +1,10 @@
+import { ApiOneResponse } from "../../../common/interfaces/api-response.interface";
+import { ResponseAuthContract } from "../contracts/response-auth.contract";
+import { SignInContract } from "../contracts/sign-in.contract";
+import { SignUpContract } from "../contracts/sign-up.contract";
+
+export abstract class AuthDatasource {
+    abstract signIn(signIn: SignInContract): Promise<ApiOneResponse<ResponseAuthContract>>;
+    abstract signUp(signUp: SignUpContract): Promise<ApiOneResponse<ResponseAuthContract>>;
+    abstract refreshToken(token: string): Promise<string>;
+}
