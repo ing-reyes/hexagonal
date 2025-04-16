30c4db5598ff69eb3a4a016fdda18bc7878a8387
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-15 21:39:42 -0400

    Refactor CreateUserDto to improve error handling by returning structured error messages

--- src/users/application/dtos/create-user.dto.ts
+++ src/users/application/dtos/create-user.dto.ts
@@ -1,3 +1,5 @@
+import { addErrorDto } from "../../../common/errors/add.error"
+
 export class CreateUserDto {
     private constructor(
         public name: string,
@@ -5,12 +7,24 @@ export class CreateUserDto {
         public password: string,
     ) {}
 
-    static create( object: {[key:string]:any} ): [string?, CreateUserDto?]{
+    static create( object: {[key:string]:any} ): [Record<string, string[]>?, CreateUserDto?]{
         const { name, email, password } = object
 
-        if( !name ) return ['Missing name'] 
-        if( !email ) return ['Missing email'] 
-        if( !password ) return ['Missing password'] 
+        let errors: Record<string, string[]> = {}
+
+        if( !name ) {
+            errors = addErrorDto('name', 'name is required', errors);
+        } 
+        if( !email ) {
+            errors = addErrorDto('email', 'email is required', errors);
+        }
+        if( !password ) {
+            errors = addErrorDto('password', 'password is required', errors);
+        }
+
+        if(Object.keys(errors).length > 0){
+            return [errors, undefined];
+        }
         
 
         return [undefined, new CreateUserDto(name, email, password)]
