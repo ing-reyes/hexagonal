0e66223c9981493c8e0d79cabe518385f7cdf5fd
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-15 21:12:22 -0400

    Refactor SignInDto and SignUpDto to improve error handling with structured error messages

--- src/auth/application/dtos/sign-in.dto.ts
+++ src/auth/application/dtos/sign-in.dto.ts
@@ -1,3 +1,5 @@
+import { addErrorDto } from "../../../common/errors/add.error";
+
 export class SignInDto{
 
     constructor(
@@ -5,11 +7,20 @@ export class SignInDto{
         public password: string,
     ){}
 
-    static signIn(object: {[key:string]:any}): [string?, SignInDto?]{
+    static signIn(object: {[key:string]:any}): [Record<string, string[]>?, SignInDto?]{
         const { email, password } = object;
 
-        if(!email) return ["email is required"];
-        if(!password) return ["password is required"];
+        let errors: Record<string,string[]> = {};
+
+        if (!email) {
+            errors = addErrorDto('email', 'email is required', errors);
+        }
+        if (!password) {
+            errors = addErrorDto('password', 'password is required', errors);
+        }
+        if (Object.keys(errors).length > 0) {
+            return [ errors, undefined ]
+        }
 
         return [ undefined, new SignInDto(email, password) ]
     }
--- src/auth/application/dtos/sign-up.dto.ts
+++ src/auth/application/dtos/sign-up.dto.ts
@@ -1,3 +1,5 @@
+import { ValidatorsAdapter } from "../../../common/adapters/validators.adapter";
+import { addErrorDto } from "../../../common/errors/add.error";
 export class SignUpDto{
 
     constructor(
@@ -6,13 +8,31 @@ export class SignUpDto{
         public password: string,
     ){}
 
-    static signUp(object: {[key:string]:any}): [string?, SignUpDto?]{
+    static signUp(object: {[key:string]:any}): [Record<string, string[]>?, SignUpDto?]{
         const { name, email, password } = object;
 
-        if(!name) return ["name is required"];
-        if(!email) return ["email is required"];
-        if(!password) return ["password is required"];
+        let errors: Record<string, string[]> = {};
 
+        if(!name) {
+            errors = addErrorDto("name", "name is required", errors);
+        }
+
+        if(!email) {
+            errors = addErrorDto("email", "email is required", errors);
+        }
+
+        if ( !ValidatorsAdapter.isEmail(email) ) {
+            errors = addErrorDto("email", "email is invalid", errors);
+        }
+
+        if(!password) {
+            errors = addErrorDto("password", "password is required", errors);
+        }
+        
+        if (Object.keys(errors).length > 0) {
+            return [ errors, undefined ];
+        }
+        
         return [ undefined, new SignUpDto(name, email, password) ];
     }
 }
