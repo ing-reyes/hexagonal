9c66b6fbd07b67a3d6705dc92aad4640ded75ba2
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-16 02:17:22 -0400

    Refactor error handling in Handler class to improve response structure and logging

--- src/common/errors/handler.error.ts
+++ src/common/errors/handler.error.ts
@@ -1,23 +1,25 @@
 import { Response } from "express";
-
-import { Logger } from "../logging/logger";
-import { ManagerError } from "./manager.error";
 import { injectable } from "inversify";
+
+import { ManagerError } from "./manager.error";
 import { SaveLogger } from "../logging/save-logger";
 
 @injectable()
 export class Handler {
-    private readonly logger = new Logger(Handler.name);
     private readonly saveLogger = new SaveLogger(Handler.name);
 
     error(error: unknown, res: Response) {
         if (error instanceof ManagerError) {
-            res.status(error.statusCode).json({ statusCode: error.statusCode, message: error.message, statusMsg: error.statusMsg });
+            res.status(error.statusCode).json({ statusCode: error.statusCode, error: error.message, statusMsg: error.statusMsg });
             return;
         }
 
-        this.logger.error(error);
+        if(error instanceof Error){
+            res.status(500).json({ statusCode: 500, error: error.message, statusMsg: 'INTERNAL_SERVER_ERROR' });
+            return;
+        }
+        
         this.saveLogger.error(error, 'handler.error.ts');
-        res.status(500).json({ statusCode: 500, message: 'Internal Server Error', statusMsg: 'INTERNAL_SERVER_ERROR' });
+        res.status(500).json({ statusCode: 500, error: 'INTERNAL_SERVER_ERROR', statusMsg: 'INTERNAL_SERVER_ERROR' });
     }
 }
