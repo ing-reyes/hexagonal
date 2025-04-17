c6004d5480e82c844ea391a128a8ff89129821e6
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-04-16 20:31:57 -0400

    Refactor UsersMiddleware to improve code readability and consistency

--- src/users/infraestructure/presentation/middlewares/users.middleware.ts
+++ src/users/infraestructure/presentation/middlewares/users.middleware.ts
@@ -5,6 +5,7 @@ import { injectable } from "inversify";
 
 @injectable()
 export class UsersMiddleware {
+    
     validateCreateUser = (req: Request, res: Response, next: NextFunction) => {
         const [error, createUserDto] = CreateUserDto.create(req.body);
         if (error) {
@@ -15,21 +16,26 @@ export class UsersMiddleware {
         //* Add the createUserDto to the request body for later use in the controller
         req.body.createUserDto = createUserDto;
 
-        next()
+        next();
 
     }
 
     validateUpdateUser = (req: Request, res: Response, next: NextFunction) => {
-        const [error, updateUserDto] = UpdateUserDto.update(req.body);
+        const {id} = req.params;
+
+        const user = req['user'];
+        
+        const [error, updateUserDto] = UpdateUserDto.update({...req.body, id, user });
         if (error) {
             res.status(400).json({ statusCode: 400, statusMsg: 'BAD_REQUEST', error: error });
             return;
         }
 
+        
+
         //* Add the updateUserDto to the request body for later use in the controller
         req.body.updateUserDto = updateUserDto;
 
-        next()
-
+        next();
     }
 }
